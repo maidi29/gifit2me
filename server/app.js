b8f99c453b6750c123d39193075a2d14b6e4b06f
Mareike <mareike.haug@web.de>
2022-10-13 23:48:41 +0200

    setup

--- server/app.js
+++ server/app.js
@@ -1,30 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const io = require('socket.io')();
 require('./socket')(io)
 
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
+const buildPath = path.join(__dirname, '../client/dist', 'client');
+app.set('views', buildPath);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(logger("dev"));
+app.use(express.static(buildPath));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
