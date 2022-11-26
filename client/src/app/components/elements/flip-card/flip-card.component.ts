2663ec33ac695ba92bfae713f0fdd8783f5317ac
Mareike <mareike.haug@web.de>
2022-11-26 15:34:14 +0100

    flip-card

---
+++ client/src/app/components/elements/flip-card/flip-card.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-flip-card',
+  templateUrl: './flip-card.component.html',
+  styleUrls: ['./flip-card.component.scss']
+})
+export class FlipCardComponent implements OnInit {
+  @Input() flipped = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public flip() {
+    this.flipped = !this.flipped;
+  }
+
+}
