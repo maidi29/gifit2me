ef505c4ffffa41b9cf0e8c16655e8e40bfe60e5f
Mareike <mareike.haug@web.de>
2023-01-23 00:30:27 +0100

    padding right

--- client/src/app/components/elements/input/input.component.ts
+++ client/src/app/components/elements/input/input.component.ts
@@ -15,6 +15,7 @@ import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 export class InputComponent implements ControlValueAccessor {
   @Input() placeholder: string = "";
   @Input() errorMessage?: string;
+  @Input() paddingRight?: number;
 
   private innerValue: string = '';
   public onChange: (value: string) => void = () => {};
